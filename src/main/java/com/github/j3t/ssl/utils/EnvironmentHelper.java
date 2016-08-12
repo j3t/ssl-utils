e5b76902397ff4d21d9c1159997554edda964a9b
j3t <jens.thielscher@gmail.com>
2016-08-12 14:29:36 +0200

    improved readability

--- src/main/java/com/github/j3t/ssl/utils/EnvironmentHelper.java
+++ src/main/java/com/github/j3t/ssl/utils/EnvironmentHelper.java
@@ -14,22 +14,22 @@ public class EnvironmentHelper
 
     public static boolean isWindows()
     {
-        return (OS.indexOf("win") >= 0);
+        return OS.indexOf("win") >= 0;
     }
 
     public static boolean isMac()
     {
-        return (OS.indexOf("mac") >= 0);
+        return OS.indexOf("mac") >= 0;
     }
 
     public static boolean isUnix()
     {
-        return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0);
+        return OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0;
     }
 
     public static boolean isSolaris()
     {
-        return (OS.indexOf("sunos") >= 0);
+        return OS.indexOf("sunos") >= 0;
     }
 
     public static boolean isJava6()
