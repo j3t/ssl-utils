41e0715a946bfddc058330ecfb3fcf83ebffce87
j3t <jens.thielscher@gmail.com>
2017-07-29 17:54:19 +0200

    removed openjdk6 build (maybe it will be added it again later)

--- add_coveralls_to_cacerts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/nash
-
-openssl s_client -connect coveralls.io:443 -servername coveralls.io 2> /dev/null < /dev/null | openssl x509 > /tmp/coveralls.io.cer
-sudo keytool -import -v -trustcacerts -alias coveralls.io -file /tmp/coveralls.io.cer -keystore $JAVA_HOME/jre/lib/security/cacerts -keypass changeit -storepass changeit -noprompt
